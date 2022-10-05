c63d9e6346324ea6aee7cf62b12cd5dde479e43f
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-10-05 12:12:42 +0200

    Moved controller to root

--- src/main/java/no/fintlabs/AuthController.java
+++ src/main/java/no/fintlabs/AuthController.java
@@ -3,10 +3,8 @@ package no.fintlabs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -21,7 +19,7 @@ public class AuthController {
         this.authorizedClientService = authorizedClientService;
     }
 
-    @GetMapping("auth")
+    @GetMapping()
     public Mono<ResponseEntity<Void>> auth() {
         log.info("");
 
