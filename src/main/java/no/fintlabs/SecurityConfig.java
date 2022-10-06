b3d29caf6eb8199ff668b43731a51ca72cf288ec
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-10-06 09:27:33 +0200

    Arrrgh

--- src/main/java/no/fintlabs/SecurityConfig.java
+++ src/main/java/no/fintlabs/SecurityConfig.java
@@ -1,12 +1,18 @@
 package no.fintlabs;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 import org.springframework.web.server.session.CookieWebSessionIdResolver;
 import org.springframework.web.server.session.WebSessionIdResolver;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -17,7 +23,7 @@ public class SecurityConfig {
 //                .pathMatchers("/**")
 //                .permitAll()
                 .anyExchange().authenticated()
-                .and().oauth2Login(Customizer.withDefaults())
+                .and().oauth2Login(withDefaults())
                 .build();
     }
 
@@ -31,4 +37,9 @@ public class SecurityConfig {
         //resolver.addCookieInitializer((builder) -> builder.secure(true));
         return resolver;
     }
+
+//    @Bean
+//    public ForwardedHeaderTransformer forwardedHeaderTransformer() {
+//        return new ForwardedHeaderTransformer();
+//    }
 }
