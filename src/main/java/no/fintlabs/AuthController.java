03cf414222d1cfed79ef8bcaa103a1cb18f7022c
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-10-07 15:26:35 +0200

    wip

--- src/main/java/no/fintlabs/AuthController.java
+++ src/main/java/no/fintlabs/AuthController.java
@@ -1,13 +1,13 @@
 package no.fintlabs;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
@@ -16,27 +16,32 @@ import java.util.Map;
 @RestController
 public class AuthController {
 
-    private final ReactiveOAuth2AuthorizedClientService authorizedClientService;
+private final WebClient webClient;
 
-    public AuthController(ReactiveOAuth2AuthorizedClientService authorizedClientService) {
-        this.authorizedClientService = authorizedClientService;
+    public AuthController(WebClient webClient) {
+        this.webClient = webClient;
     }
 
     @GetMapping("auth")
-    public Mono<ResponseEntity<Void>> auth(@RequestHeader Map<String, String> headers) {
+    public Mono<ResponseEntity<String>> auth(@RequestHeader Map<String, String> headers, ServerHttpResponse response, ServerHttpRequest request) {
         log.info("Request headers:");
         headers.forEach((s, s2) -> log.info("\t{}: {}", s, s2));
 
-        return ReactiveSecurityContextHolder.getContext()
-                .map(securityContext -> securityContext.getAuthentication().getName())
-                .flatMap(this::getAccessToken)
-                .map(token -> ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, "Bearer " + token).build());
-    }
 
-    private Mono<String> getAccessToken(String name) {
-        return authorizedClientService
-                .loadAuthorizedClient("fint", name)
-                .map(client -> client.getAccessToken().getTokenValue());
+//        Cookie cookie = new Cookie();
+//        cookie.setDomain("frode");
+//        cookie.setHttpOnly(true);
+//        cookie.setPath("/");
+//        ResponseCookie build = ResponseCookie.from("test", UUID.randomUUID().toString())
+//                //.domain(r)
+//                .httpOnly(true)
+//                .secure(request.getURI().getScheme().equalsIgnoreCase("https"))
+//                .path("/")
+//                .build();
+//        response.addCookie(build);
+//
+//        return Mono.just(ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, build.toString()).build());
+
     }
 
 
---
+++ src/main/java/no/fintlabs/WebClientConfig.java
@@ -0,0 +1,14 @@
+package no.fintlabs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create("https://idp.felleskomponent.no");
+    }
+}
